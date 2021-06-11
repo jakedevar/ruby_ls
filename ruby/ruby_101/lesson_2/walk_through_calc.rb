afe026a9967dc97f650c288864dea6599067bd9d
Steeltip <devarennesjacob@gmail.com>
2021-06-10 21:43:08 -0500

    orgainizing

---
+++ ruby/ruby_101/lesson_2/walk_through_calc.rb
@@ -0,0 +1,107 @@
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+prompt("Welcome to Calculator! Enter your name:")
+
+name = ''
+  loop do
+    name =gets.chomp
+    if name.empty?
+      prompt("Make sure to use a valid name.")
+    else
+      break
+    end
+  end
+
+prompt("Hi #{name}!")
+
+loop do #main loop
+
+ 
+  def valid_number?(num)
+    num.to_i.to_s == num
+  end
+
+  number1 = nil
+  loop do
+  prompt("What's the first number?")
+  number1 = gets.chomp
+    if valid_number?(number1)
+      break
+    else
+    prompt("Hmm.. that dosen't look like a valid number")
+    end
+  end
+
+  number2 = nil
+  loop do 
+  prompt("What's the second number?")
+  number2 = gets.chomp
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm.. that dosen't look like a vlaid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = nil
+  loop do
+    operator = gets.chomp
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3, or 4")
+    end
+  end
+
+  def operation_to_message(op)
+    case op
+    when '1'
+      return 'Adding'
+    when '2'
+      return 'Subtracting'
+    when '3'
+      return 'Multiplying'
+    when '4'
+      return 'Dividing'
+    end
+    puts 'nioce'
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+  when '1'
+    number1.to_i + number2.to_i
+  when '2'
+    number1.to_i - number2.to_i
+  when '3'
+    number1.to_i * number2.to_i
+  when '4'
+    number1.to_f / number2.to_f
+  end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want t operform another calculation? (Y to calcuate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("thank you for using claculator")
