7155ce63aba016a6d88c043f60b1017b6cd1b294
Steeltip <jakedevar111@gmail.com>
2021-06-16 19:46:22 -0500

    from work

---
+++ lesson_3/exercise_2.rb
@@ -0,0 +1,7 @@
+#exercise_2.rb
+1. != is the not operator. It flips the boolean from true to false, or it can mean a value must not equal a variable 
+2. ! basically flips the value from true to flase or false to true\
+3. putting ! after a method usually but not always means that it will mutate the caller 
+4. It will do nothing either that or it represents a ternary operator 
+5. putting ? after something usually means that it will evaluate the method to a boolean truth or false 
+6. putting !! before somehting usually flips it twice so flase will remain false and true will remain true 
