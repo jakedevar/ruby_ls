1c9531a350917071155e30d973d7b1cedf2c2baf
Steeltip <jakedevar111@gmail.com>
2021-06-18 17:17:05 -0500

    from work

---
+++ lesson_3/medium_1/exercise_3.rb
@@ -0,0 +1,12 @@
+#exercise_3.rb
+def factors(number)
+  divisor = number
+  factors = []
+  begin
+    factors << number / divisor if number % divisor = 0
+    divisor -= 1
+  end until divisor == 0
+  factors 
+end
+
+factors(10)
