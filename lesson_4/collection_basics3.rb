6410dba75a7a7b4d28e9738b12942509128cfe50
jakedevar <devarennesjacob@gmail.com>
2021-06-22 23:53:48 -0500

    from home

---
+++ lesson_4/collection_basics3.rb
@@ -0,0 +1,12 @@
+#collection_basics3.rb
+
+hsh = { apple: 'Produce', carrot: 'Produce', pear: 'Produce', broccoli: 'Produce' }
+hsh[:apple] = 'Fruit'
+hsh # => { :apple => "Fruit", :carrot => "Produce", :pear => "Produce", :broccoli => "Produce" }
+hsh[:apple] = 'Fruit'
+hsh[:carrot] = 'Vegitable'
+hsh[:pear] = 'Fruit'
+hsh[:broccoli] = 'Vegitable'
+hsh[:apple] = 'Fruit'
+
+puts hsh
