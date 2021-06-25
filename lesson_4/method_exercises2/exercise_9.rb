f5a14a64192b2e6c83ed36cce7158675683119a5
Steeltip <jakedevar111@gmail.com>
2021-06-25 18:09:57 -0500

    'from work'

---
+++ lesson_4/method_exercises2/exercise_9.rb
@@ -0,0 +1,16 @@
+#exercise_9.rb
+words = "the flintstones rock"
+
+return_words = ''
+
+def capitalize_words(the_words, container)
+  split_string = the_words.split
+  caps_words = split_string.map! { |word| word.capitalize }
+  caps_words.each { |tword| container << tword + ' '}
+end
+  
+capitalize_words(words, return_words)
+p return_words
+
+#or the more elegant LS solution
+words.split.map { |word| word.capitalize }.join(' ')
