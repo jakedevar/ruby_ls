c2b7c5537f4fb398bc7b2a4886e4bf4a1c9aa4dd
Steeltip <jakedevar111@gmail.com>
2021-07-19 19:35:24 -0500

    'from work'

--- small_problems/easy5/exercise_5.rb
+++ small_problems/easy5/exercise_5.rb
@@ -1,16 +1,14 @@
 #exercise_5.rb
-ALPHABET = ('a'..'z').to_a
 
 def cleanup(string)
-  char_storage = []
-  
+  storage_arr = []
+  string.gsub!(/[\W]/, ' ')
   string.chars.each do |char|
-  if ALPHABET.include?(char)
-    char_storage << char
-  else
-    char_storage << ' ' unless char_storage.last == ' '
+    storage_arr << char unless char == storage_arr.last
   end
-  end
+  storage_arr.flatten.join
 end
 
 p cleanup("---what's my +*& line?") == ' what s my line '
+
+#retried this one because I saw it had been marked not done. Man after learning a whole lot and coming back this one was easy peasy!!
