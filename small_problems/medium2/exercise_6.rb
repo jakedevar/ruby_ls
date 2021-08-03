2236805e5577a00800dcac0ce46e76b28e3a2b23
Steeltip <devarennesjacob@gmail.com>
2021-08-02 21:59:45 -0500

    from work

--- small_problems/medium2/exercise_6.rb
+++ small_problems/medium2/exercise_6.rb
@@ -1,3 +1,21 @@
 #exercise_6.rb
 
-hey buddy review the souttion for the prevoius exercise on this one i think it will hlep alot. was at work for this one and felt like i coulent brain like that
+def triangle?(one ,two, three)
+  arr = [one, two, three].sort
+  arr.each do |int|
+    if int == 0
+      return :invalid
+    elsif arr.inject(:+) != 180
+      return :invalid
+    end
+  end
+end
+
+def triangle(one, two, three)
+  if triangle?(one, two, three) && 
+
+p triangle(60, 70, 50) #== :acute
+p triangle(30, 90, 60) #== :right
+p triangle(120, 50, 10) #== :obtuse
+p triangle(0, 90, 90) #== :invalid
+p triangle(50, 50, 50) #== :invalid
--- x.rb
+++
@@ -1,8 +0,0 @@
-def joinor(arr, symbol = ', ', conjunction = ' or ')
-  arr[-1] = (symbol + conjunction + arr[-1].to_s)
-end
-
-p joinor([1, 2])                   # => "1 or 2"
-p joinor([1, 2, 3])                # => "1, 2, or 3"
-#joinor([1, 2, 3], '; ')          # => "1; 2; or 3"
-#joinor([1, 2, 3], ', ', 'and')   # => "1, 2, and 3"
