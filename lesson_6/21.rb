c48a4759b3a893e5fa0b58d7b0aec0a1c1ffc8cd
Steeltip <devarennesjacob@gmail.com>
2021-08-06 21:31:00 -0500

    from home

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -1,77 +1,91 @@
-require 'pry'
-CARDS = { 'Ace'=>11, 'Two'=>2, 'Three'=>3, 'Four'=>4, 'Five'=>5, 'Six'=>6, 'Seven'=>7, 'Eight'=>8, 'Nine'=>9, 'Ten'=>10, 'Jack'=>10, 'Queen'=>10, 'King'=>10 }
-RAND = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-PLAYER = []
-COMPUTER = []
+# frozen_string_literal: true
+
+CARDS = { 'Ace' => 11, 'Two' => 2, 'Three' => 3, 'Four' => 4, 'Five' => 5, 'Six' => 6, 'Seven' => 7, 'Eight' => 8, 'Nine' => 9,
+          'Ten' => 10, 'Jack' => 10, 'Queen' => 10, 'King' => 10 }.freeze
+RAND = %w[Ace Two Three Four Five Six Seven Eight Nine Ten Jack Queen King].freeze
+PLAYER = [].freeze
+COMPUTER = [].freeze
 
 def prompt(string)
   puts ">> #{string}"
 end
 
 def deal_conversion(arr)
-  arr.map {|ele| CARDS[ele]}
+  arr.map { |ele| CARDS[ele] }
 end
 
-def player_cards(deck)
+def player_cards(_deck)
   PLAYER << RAND.sample
 end
 
-def computer_cards(deck)
+def computer_cards(_deck)
   COMPUTER << RAND.sample
 end
 
 def display_cards(player, computer)
   prompt("Computer has: #{computer[0]}, Unknown")
-  prompt("Player has: #{player.join(', ')}. Total: #{deal_conversion(PLAYER).sum}")
+  if PLAYER.include?('Ace')
+    prompt("Player has: #{player.join(', ')}")
+  else
+    prompt("Player has: #{player.join(', ')}.")
+  end
 end
 
 def computer_display_cards(player, computer)
-  prompt("Computer has: #{computer.join(', ')} Total: #{deal_conversion(computer).sum}")
-  prompt("Player has: #{player.join(', ')}. Total: #{deal_conversion(player).sum}")
+  prompt("Computer has: #{computer.join(', ')}")
+  prompt("Player has: #{player.join(', ')}.")
 end
 
-loop do 
+loop do
   prompt('Lets begin!')
   person = nil
   dealer = nil
   loop do
     break if COMPUTER.size >= 2
+
     player_cards(CARDS)
     computer_cards(CARDS)
   end
 
   display_cards(PLAYER, COMPUTER)
-  #, "Ten", "Jack", "Queen", "King"
+
   loop do
-    prompt("Player: Hit or Stay? (H/S)")
+    prompt('Player: Hit or Stay? (H/S)')
     player_input = gets.chomp.downcase
     break if player_input == 's'
+
     if player_input == 'h'
       player_cards(CARDS)
+      display_cards(PLAYER, COMPUTER)
       if deal_conversion(PLAYER).sum > 21 && !PLAYER.include?('Ace')
-        display_cards(PLAYER, COMPUTER)
         prompt('You busted!!!! Computer wins...')
         person = :bust
         break
-      elsif deal_conversion(PLAYER).sum > 10 && PLAYER.include?('Ace')
-        (deal_conversion(PLAYER).sum - 10) > 21
-        display_cards(PLAYER, COMPUTER)
-        prompt('You busted!!!! Computer wins...')
-        person = :bust
-      
-      else
-        display_cards(PLAYER, COMPUTER)
+      elsif deal_conversion(PLAYER).sum > 21 && PLAYER.include?('Ace')
+        if deal_conversion(PLAYER).sum - (10 * PLAYER.count('Ace')) > 21
+          display_cards(PLAYER, COMPUTER)
+          prompt('You busted!!!! Computer wins...')
+          person = :bust
+          break
+        end
       end
-    else 
+    else
       prompt('That is not a valid input please enter H or S')
     end
   end
-  
+
   unless person == :bust
-   loop do 
+    loop do
       computer_display_cards(PLAYER, COMPUTER)
       if deal_conversion(COMPUTER).sum <= 17
         computer_cards(CARDS)
+      elsif deal_conversion(COMPUTER).sum > 21 && COMPUTER.include?('Ace')
+        if deal_conversion(COMPUTER).sum - (10 * COMPUTER.count('Ace')) > 21
+          display_cards(PLAYER, COMPUTER)
+          prompt('Computer busts...You win!!!!')
+          person = :bust
+          break
+        end
       elsif deal_conversion(COMPUTER).sum > 21
         prompt('Computer busts...You win!!!!')
         dealer = :bust
@@ -82,16 +96,16 @@ loop do
     end
   end
   unless person == :bust || dealer == :bust
-    if (21 - deal_conversion(PLAYER).sum) == (21 - deal_conversion(COMPUTER).sum)
-      prompt('It\'s a draw!')
+    if (21 - deal_conversion(COMPUTER).sum) < (21 - deal_conversion(PLAYER).sum)
+      prompt('Computer wins...')
     elsif (21 - deal_conversion(PLAYER).sum) < (21 - deal_conversion(COMPUTER).sum)
       prompt('Player wins!!!!')
     else
-      prompt('Computer wins...')
+      prompt('It\'s a draw!')
     end
   end
-  PLAYER.delete_if { |ele| ele != 0}
-  COMPUTER.delete_if { |ele| ele != 0}
+  PLAYER.delete_if { |ele| ele != 0 }
+  COMPUTER.delete_if { |ele| ele != 0 }
   prompt('Want to play again? (Y/N)')
   play_again = gets.chomp.downcase
   break if play_again != 'y'
--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -9,31 +11,30 @@ end
 
 def display_board(brd)
   puts "You are the #{PLAYER_MARKER} the computer is #{COMPUTER_MARKER}"
-puts ""
-puts "     |     |"
-puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-puts "     |     |"
-puts "-----+-----+------"
-puts "     |     |"
-puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-puts "     |     |"
-puts "-----+-----+------"
-puts "     |     |"
-puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-puts "     |     |"
-puts ""
+  puts ''
+  puts '     |     |'
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts '     |     |'
+  puts '-----+-----+------'
+  puts '     |     |'
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts '     |     |'
+  puts '-----+-----+------'
+  puts '     |     |'
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts '     |     |'
+  puts ''
 end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
-
 
 def player_places_peice!(brd)
   square = ''
@@ -41,6 +42,7 @@ def player_places_peice!(brd)
     prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
+
     prompt "Sorry, that's not a valid choice"
   end
   brd[square] = PLAYER_MARKER
@@ -60,31 +62,32 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] + #rows
-                  [[1,4,7], [2,5,8], [3,6,9]] + #colums
-                  [[1,5,9], [3,5,7]]  #diagonals
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
+                  [[1, 5, 9], [3, 5, 7]]  # diagonals
   winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER && 
+    if brd[line[0]] == PLAYER_MARKER &&
        brd[line[1]] == PLAYER_MARKER &&
        brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER && 
+      return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
           brd[line[1]] == COMPUTER_MARKER &&
           brd[line[2]] == COMPUTER_MARKER
-      
+
       return 'Computer'
-    
+
     end
   end
   nil
 end
-loop do 
+loop do
   board = initialize_board
 
   loop do
     display_board(board)
     player_places_peice!(board)
     break if someone_won?(board) || board_full?(board)
+
     computer_places_peice(board)
     break if someone_won?(board) || board_full?(board)
   end
@@ -97,18 +100,19 @@ loop do
     prompt "It's a tie!"
   end
 
-
-  if detect_winner(board) == 'Player'
+  case detect_winner(board)
+  when 'Player'
     wins_player += 1
-  elsif detect_winner(board) == 'Computer'
+  when 'Computer'
     wins_computer += 1
   end
 
-  if (wins_player == 5 || wins_computer == 5)
+  if wins_player == 5 || wins_computer == 5
     break
-  elsif (wins_player < 5 || wins_computer < 5)
+  elsif wins_player < 5 || wins_computer < 5
     prompt("Want to play again? Y/N (Player: #{wins_player} Computer: #{wins_computer})")
     play_again = gets.chomp
   end
+
   display_board(board)
 end
