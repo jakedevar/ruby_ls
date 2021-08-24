fc43f55610d7a3e873ff6795327fd678f62c75b5
jakedevar <devarennesjacob@gmail.com>
2021-08-24 17:30:05 -0500

    from work

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 CARDS = { 'Ace' => 11, 'Two' => 2, 'Three' => 3, 'Four' => 4, 'Five' => 5, 'Six' => 6, 'Seven' => 7, 'Eight' => 8, 'Nine' => 9,
-          'Ten' => 10, 'Jack' => 10, 'Queen' => 10, 'King' => 10 }.freeze
-RAND = %w[Ace Two Three Four Five Six Seven Eight Nine Ten Jack Queen King].freeze
-PLAYER = [].freeze
-COMPUTER = [].freeze
+          'Ten' => 10, 'Jack' => 10, 'Queen' => 10, 'King' => 10 }
+RAND = %w[Ace Two Three Four Five Six Seven Eight Nine Ten Jack Queen King]
+PLAYER = []
+COMPUTER = []
 
 def prompt(string)
   puts ">> #{string}"
@@ -20,6 +20,10 @@ end
 
 def computer_cards(_deck)
   COMPUTER << RAND.sample
+end
+
+def total(cards)
+  puts cards.sum
 end
 
 def display_cards(player, computer)
