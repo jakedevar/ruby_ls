1d49cc545960187f0692d6dfbb0f10635673c6d1
Steeltip <devarennesjacob@gmail.com>
2021-08-28 23:31:48 -0500

    from home

--- lesson_6/ttt_bonus.rb
+++ lesson_6/ttt_bonus.rb
@@ -6,7 +6,7 @@ COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
                 [[1, 5, 9], [3, 5, 7]]
-COMPUTER_CHOICE = %w[c h].freeze
+COMPUTER_CHOICE = %w[c h]
 wins_player = 0
 wins_computer = 0
 
