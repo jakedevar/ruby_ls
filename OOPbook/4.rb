1bedcf2d79a081618aa9bf09b666ab9582e3c468
jakedevar <devarennesjacob@gmail.com>
2021-10-18 09:46:03 +0000

    from laptop

--- OOPbook/4.rb
+++ OOPbook/4.rb
@@ -1,7 +1,45 @@
-class GoodDog
-  def initialize(name)
-    @name = name
+class MyCar
+attr_accessor :color, :model
+attr_reader :year
+  def self.calculate_mpg(gas_difference, miles_drive)
+    miles_drive / gas_difference
+  end
+
+  def initialize(year, color, model)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def speed_up(speed)
+    @current_speed += speed
+    puts "You push the gas and accelerate to #{@current_speed} mph."
+  end
+
+  def brake(speed)
+    @current_speed -= speed
+  end
+  
+  def current_speed
+    puts "You are now going #{@current_speed} mph"
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint=(color)
+    @color = color
+  end
+
+  def to_s
+    "this #{model} is fast"
   end
 end
 
-sparky = GoodDog.new("Sparky")
+chevy = MyCar.new('2017', 'white', 'malibu')
+
+puts MyCar.calculate_mpg(3, 122)
+puts chevy
